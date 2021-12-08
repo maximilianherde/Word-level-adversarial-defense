d7fc42030e9f10550c24e92872d2d265519e8da0
Mert Erkul <72577290+merterkul98@users.noreply.github.com>
2021-12-08 23:09:35 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,10 +2,11 @@ from torch.utils.data import Dataset
 
 
 class ClassificationDataset(Dataset):
-    def __init__(self, dataset, num_classes, tokenizer):
+    def __init__(self, dataset, num_classes, tokenizer, model):
         self.num_classes = num_classes
         self.dataset = dataset
         self.tokenizer = tokenizer
+        self.model = model
 
     def __len__(self):
         return self.dataset.__len__()
@@ -19,4 +20,8 @@ class ClassificationDataset(Dataset):
                 label = 1
         else:
             label = int(label) - 1
-        return label, self.tokenizer(text)
+
+        if self.model == 'BERT':
+            return label, self.tokenizer(text, padding="max_length", return_tensors='pt', max_length=512, truncation=True)
+        else:
+            return label, self.tokenizer(text)
