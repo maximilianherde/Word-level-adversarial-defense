f186335a41fa43ce1445e45b88c5c95f5aab080c
Maximilian Herde <herdem@student.ethz.ch>
2021-12-19 13:51:40 +0100

    IMDB dataset euler

--- datasets_euler.py
+++ datasets_euler.py
@@ -1,9 +1,11 @@
 from torch.utils.data import Dataset
 import pandas as pd
+import io
+import os
 from torchtext.data.utils import get_tokenizer
 from WLADL import mask_replace_with_syns_add_noise
 
-BASIC_PATH = '~/.torchtext/cache'
+BASIC_PATH = '/cluster/scratch/herdem'
 
 
 class AG_NEWS(Dataset):
@@ -51,11 +53,39 @@ class IMDB(Dataset):
             self.thesaurus = thesaurus
             self.embedding = embedding
 
+        def yield_data(label, files):
+            for file in files:
+                with io.open(file, encoding='utf8') as f:
+                    yield label, f.read()
+        if split == 'train':
+            filelist_pos = os.listdir(
+                BASIC_PATH + '/IMDB/aclImdb/' + split + '/pos')
+            filelist_neg = os.listdir(
+                BASIC_PATH + '/IMDB/aclImdb/' + split + '/neg')
+        elif split == 'test':
+            filelist_pos = os.listdir(
+                BASIC_PATH + '/IMDB/aclImdb/' + split + '/pos')
+            filelist_neg = os.listdir(
+                BASIC_PATH + '/IMDB/aclImdb/' + split + '/neg')
+        else:
+            raise ValueError()
+        df_pos = pd.DataFrame(yield_data(1, filelist_pos),
+                              index=['label', 'data'])
+        df_neg = pd.DataFrame(yield_data(0, filelist_neg),
+                              index=['label', 'data'])
+        self.dataset = pd.concat([df_pos, df_neg])
+
     def __len__(self):
-        pass
+        return len(self.dataset.index)
 
     def __getitem__(self, idx):
-        pass
+        if self.model == 'BERT':
+            if self.with_defense:
+                return int(self.dataset.iloc[idx, 0]) - 1, self.tokenizer(' '.join(mask_replace_with_syns_add_noise(self.temp_tokenizer(' '.join((self.dataset.iloc[idx, 1:]).map(str))), self.thesaurus, self.embedding, self.model)), padding='max_length', return_tensors='pt', max_length=512, truncation=True)
+            else:
+                return int(self.dataset.iloc[idx, 0]) - 1, self.tokenizer(' '.join((self.dataset.iloc[idx, 1:]).map(str)), padding='max_length', return_tensors='pt', max_length=512, truncation=True)
+        else:
+            return int(self.dataset.iloc[idx, 0]) - 1, self.tokenizer(' '.join((self.dataset.iloc[idx, 1:]).map(str)))
 
 
 class YahooAnswers(Dataset):
