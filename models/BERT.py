e93b3a6ad8f8d779f36f8b813451089d2aafb2be
Maximilian Herde <herdem@student.ethz.ch>
2021-12-31 10:44:14 +0100

    model comments

--- models/BERT.py
+++ models/BERT.py
@@ -1,5 +1,17 @@
+"""
+
+A BERT sequence classifier.
+
+"""
+
+
 import torch.nn as nn
 from transformers import BertForSequenceClassification
+
+##########################################################################
+# helper functions for fine-tuning last two layers of encoder and pooling
+##########################################################################
+
 
 def get_child(model, *arg):
     res = model
@@ -7,42 +19,56 @@ def get_child(model, *arg):
         res = list(res.children())[i]
     return res
 
+
 def freeze_model(model):
     for param in model.parameters():
-            param.requires_grad = False
-            
+        param.requires_grad = False
+
+
 def unfreeze_model(model):
     for param in model.parameters():
-            param.requires_grad = True
+        param.requires_grad = True
 
-def count_parameters(model, trainable_only = True):
+
+def count_parameters(model, trainable_only=True):
     if trainable_only:
         return sum(p.numel() for p in model.parameters() if p.requires_grad)
     else:
         return sum(p.numel() for p in model.parameters())
 
+
 def custom_freezer(model):
     unfreeze_model(model)
 
-    ## freeze whole BertLayer
+    # freeze whole BertLayer
     for c in model.children():
         if str(c).startswith('Bert'):
             freeze_model(c)
-            
-    ## unfreeze top 2 layer in BertEncoder
+
+    # unfreeze top 2 layer in BertEncoder
     bert_encoder = get_child(model, 0, 1, 0)
     for i in range(1, 3):
-        m = bert_encoder[-i] 
+        m = bert_encoder[-i]
         unfreeze_model(m)
-        
-    ## unfreeze Pooling layer
+
+    # unfreeze Pooling layer
     bert_pooling = get_child(model, 0, 2)
     unfreeze_model(bert_pooling)
 
     print('Trainable parameters: {}'.format(count_parameters(model, True)))
     return model
 
+##########################################################################
+
+
 class BERTClassifier(nn.Module):
+    """
+    A BERT sequence classifier.
+    The last two layers of the encoder as well as the pooling layer can be fine-tuned/trained.
+    Approx 15 million parameters.
+    Calculates class probabilites.
+    """
+
     def __init__(self, num_classes):
         super(BERTClassifier, self).__init__()
 
--- models/CNN.py
+++ models/CNN.py
@@ -1,3 +1,9 @@
+"""
+
+Contains the Convolutional Neural Networks for document classification.
+
+"""
+
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear, Softmax, Conv2d, Dropout
@@ -5,26 +11,37 @@ import torch.nn as nn
 
 
 class CNNClassifier(torch.nn.Module):
+    """
+    A CNN classifier using 2D-convolutions.
+    Calculates class probabilities.
+    """
+
     def __init__(self, num_classes, in_channels, out_channels, kernel_heights, pad=0, stri=1, embed_dim=50, drop=0.2):
         super().__init__()
-        self.conv1 = Conv2d(in_channels, out_channels[0], kernel_size=(kernel_heights[0], embed_dim), stride=stri, padding=pad)
-        self.conv2 = Conv2d(in_channels, out_channels[1], kernel_size=(kernel_heights[1], embed_dim), stride=stri, padding=pad)
-        self.conv3 = Conv2d(in_channels, out_channels[2], kernel_size=(kernel_heights[2], embed_dim), stride=stri, padding=pad)
+        self.conv1 = Conv2d(in_channels, out_channels[0], kernel_size=(
+            kernel_heights[0], embed_dim), stride=stri, padding=pad)
+        self.conv2 = Conv2d(in_channels, out_channels[1], kernel_size=(
+            kernel_heights[1], embed_dim), stride=stri, padding=pad)
+        self.conv3 = Conv2d(in_channels, out_channels[2], kernel_size=(
+            kernel_heights[2], embed_dim), stride=stri, padding=pad)
         self.drop = Dropout(drop)
         self.fc = Linear(sum(out_channels), num_classes)
         self.soft = Softmax(dim=1)
 
     def _conv_n_maxpool_1d(self, input, conv_layer):
 
-        conved = conv_layer(input) # conved.size() = (batch_size, out_channels[0], dim, 1)
-        reld = F.relu(conved.squeeze(3)) # reld.size() = (batch_size, out_channels[0], dim)
-        max_out = F.max_pool1d(reld, reld.size()[2]).squeeze(2) # maxpool_out.size() = (batch_size, out_channels[0])
+        # conved.size() = (batch_size, out_channels[0], dim, 1)
+        conved = conv_layer(input)
+        # reld.size() = (batch_size, out_channels[0], dim)
+        reld = F.relu(conved.squeeze(3))
+        # maxpool_out.size() = (batch_size, out_channels[0])
+        max_out = F.max_pool1d(reld, reld.size()[2]).squeeze(2)
 
         return max_out
 
     def forward(self, x):
         # x.size() = (batch_size, num_seq, embed_dim)
-        x = x.unsqueeze(1) # x.size() = (batch_size, 1, num_seq, embed_dim)
+        x = x.unsqueeze(1)  # x.size() = (batch_size, 1, num_seq, embed_dim)
 
         out_1 = self._conv_n_maxpool_1d(x, self.conv1)
         out_2 = self._conv_n_maxpool_1d(x, self.conv2)
@@ -40,11 +57,17 @@ class CNNClassifier(torch.nn.Module):
 
 
 class CNNClassifier2(torch.nn.Module):
+    """
+    A CNN classifier using 1D-convolutions.
+    Calculates class probabilites.
+    """
+
     def __init__(self, num_classes, embedding_dim=50):
         super().__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv1d(in_channels=embedding_dim, out_channels=28, kernel_size=3),
+            nn.Conv1d(in_channels=embedding_dim,
+                      out_channels=28, kernel_size=3),
             nn.MaxPool1d(kernel_size=2),
             nn.ReLU(),
             nn.Conv1d(in_channels=28, out_channels=128, kernel_size=3),
